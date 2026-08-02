218beaf2fd23b1bce9ff303842370a6dd41c99e2
salma602 <s.f.ahmed2005@gamil.com>
2026-08-02 22:49:11 +0300

    update config & main -> (OPENAI & CoHere)

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -11,6 +11,22 @@ class Settings(BaseSettings):
     MONGO_URI: str
     MONGO_DB_NAME: str
 
+
+    GENERATION_BACKEND: str
+    EMBEDDING_BACKEND: str
+
+    OPENAI_API_KEY: str = None
+    OPENAI_API_URL: str = None
+    COHERE_API_KEY: str = None
+
+    GENERATION_MODEL_ID: str = None
+    EMBEDDING_MODEL_ID: str = None
+    EMBEDDING_MODEL_SIZE: int = None
+    INPUT_DAFAULT_MAX_CHARACTERS: int = None
+    GENERATION_DAFAULT_MAX_TOKENS: int = None
+    GENERATION_DAFAULT_TEMPERATURE: float = None
+
+    
     model_config = SettingsConfigDict(
         env_file=".env",
         env_file_encoding="utf-8"
--- src/main.py
+++ src/main.py
@@ -2,10 +2,10 @@ from fastapi import FastAPI
 from motor.motor_asyncio import AsyncIOMotorClient
 from routes import base, data
 from helpers.config import get_settings
-
+from stores.LLM.LLMProviderFactory import LLMProviderFactory
 app = FastAPI()
 
-@app.on_event("startup")
+
 async def startup_db_client():
     settings = get_settings()
 
@@ -15,13 +15,21 @@ async def startup_db_client():
     app.mongodb_conn = AsyncIOMotorClient(settings.MONGO_URI)
     app.mongodb_client = app.mongodb_conn[settings.MONGO_DB_NAME]
 
-    # Test connection
-    await app.mongodb_conn.admin.command("ping")
-    print("MongoDB Connected Successfully!")
+    llm_provider_factory = LLMProviderFactory(config=settings)
+    app.generation_client = llm_provider_factory.create(provider=settings.GENERATION_BACKEND)
+    app.generation_client.set_generation_model(model_id=settings.GENERATION_MODEL_ID)
+    app.embedding_client = llm_provider_factory.create(provider=settings.EMBEDDING_BACKEND)
+    app.embedding_client.set_embedding_model(model_id=settings.EMBEDDING_MODEL_ID, embedding_size=settings.EMBEDDING_SIZE)
 
-@app.on_event("shutdown")
+
+
+    
 async def shutdown_db_client():
     app.mongodb_conn.close()
+
+
+app.router.lifespan.on_startup.append(startup_db_client)
+app.router.lifespan.on_shutdown.append(shutdown_db_client)
 
 app.include_router(base.base_router)
 app.include_router(data.data_router)
