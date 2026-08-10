baeac6711cc67861a390be55778bd48359b11684
salma602 <s.f.ahmed2005@gamil.com>
2026-08-10 11:33:59 +0300

    batch embedding

--- src/stores/LLM/providers/CoHereProvider.py
+++ src/stores/LLM/providers/CoHereProvider.py
@@ -1,7 +1,8 @@
-from ..LLMinterface import LLMInterface
+from ..LLMInterface import LLMInterface
 from ..LLMEnums import CoHereEnums, DocumentTypeEnum
 import cohere
 import logging
+from typing import List, Union
 
 class CoHereProvider(LLMInterface):
 
@@ -22,8 +23,8 @@ class CoHereProvider(LLMInterface):
         self.embedding_size = None
 
         self.client = cohere.Client(api_key=self.api_key)
-        self.enums=CoHereEnums
 
+        self.enums = CoHereEnums
         self.logger = logging.getLogger(__name__)
 
     def set_generation_model(self, model_id: str):
@@ -64,10 +65,13 @@ class CoHereProvider(LLMInterface):
         
         return response.text
     
-    def embed_text(self, text: str, document_type: str = None):
+    def embed_text(self, text: Union[str, List[str]], document_type: str = None):
         if not self.client:
             self.logger.error("CoHere client was not set")
             return None
+        
+        if isinstance(text, str):
+            text = [text]
         
         if not self.embedding_model_id:
             self.logger.error("Embedding model for CoHere was not set")
@@ -79,7 +83,7 @@ class CoHereProvider(LLMInterface):
 
         response = self.client.embed(
             model = self.embedding_model_id,
-            texts = [self.process_text(text)],
+            texts = [ self.process_text(t) for t in text ],
             input_type = input_type,
             embedding_types=['float'],
         )
@@ -88,10 +92,10 @@ class CoHereProvider(LLMInterface):
             self.logger.error("Error while embedding text with CoHere")
             return None
         
-        return response.embeddings.float[0]
+        return [ f for f in response.embeddings.float ]
     
     def construct_prompt(self, prompt: str, role: str):
         return {
             "role": role,
-            "text":prompt
+            "text": prompt,
         }
