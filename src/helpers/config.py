d9e1392527ebf80c9b63be6baae95acd34f4a449
salma602 <s.f.ahmed2005@gamil.com>
2026-08-10 10:12:47 +0300

    PGVector store class

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
@@ -23,6 +25,7 @@ class Settings(BaseSettings):
     OPENAI_API_URL: str = None
     COHERE_API_KEY: str = None
 
+    GENERATION_MODEL_ID_LITERAL: List[str] = None
     GENERATION_MODEL_ID: str = None
     EMBEDDING_MODEL_ID: str = None
     EMBEDDING_MODEL_SIZE: int = None
@@ -30,9 +33,12 @@ class Settings(BaseSettings):
     GENERATION_DEFAULT_MAX_TOKENS: int = None
     GENERATION_DEFAULT_TEMPERATURE: float = None
 
-    VECTOR_DB_BACKEND: str = None
-    VECTOR_DB_PATH: str = None
+    
+    VECTOR_DB_BACKEND_LITERAL: List[str] = None
+    VECTOR_DB_BACKEND : str
+    VECTOR_DB_PATH : str
     VECTOR_DB_DISTANCE_METHOD: str = None
+    VECTOR_DB_PGVEC_INDEX_THRESHOLD: int = 100
 
     PRIMARY_LANG: str="en"
     DEFAULT_LANG: str="en"
--- src/stores/vectorDB/VectorDBEnums.py
+++ src/stores/vectorDB/VectorDBEnums.py
@@ -1,7 +1,25 @@
 from enum import Enum
-class VectorDBEnums(str, Enum):
+
+class VectorDBEnums(Enum):
     QDRANT = "QDRANT"
+    PGVECTOR = "PGVECTOR"
 
 class DistanceMethodEnums(Enum):
-    COSINE = "COSINE"
-    DOT = "DOT"
+    COSINE = "cosine"
+    DOT = "dot"
+
+class PgVectorTableSchemeEnums(Enum):
+    ID = 'id'
+    TEXT = 'text'
+    VECTOR = 'vector'
+    CHUNK_ID = 'chunk_id'
+    METADATA = 'metadata'
+    _PREFIX = 'pgvector'
+
+class PgVectorDistanceMethodEnums(Enum):
+    COSINE = "vector_cosine_ops"
+    DOT = "vector_l2_ops"
+
+class PgVectorIndexTypeEnums(Enum):
+    HNSW = "hnsw"
+    IVFFLAT = "ivfflat"
